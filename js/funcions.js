fd7cbde01c616463b95648eb5a9ed69946df44af
MCarmen <detoro.mcarmen@gmail.com>
2025-10-02 12:29:38 +0200

    Problem solution

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,7 @@
 function confirmaRegistre(){
-    //completa
+    alert("registrant estudiant");
+    let formDiv = document.getElementById("formDiv")
+    formDiv.innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
+    console.log("registrant estudiant");
+    return false;
 }
