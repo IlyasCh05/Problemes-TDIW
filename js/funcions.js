b48554c6e35d7c7cea60943912a68b04d824f0ce
IlyasCh05 <1704353@uab.cat>
2025-10-03 20:09:44 +0200

    medio hecho

--- js/funcions.js
+++ js/funcions.js
@@ -1,3 +1,9 @@
 function confirmaRegistre(){
     //completa
+    alert("Registrant user...")
+    console.log("Registrant user...")
+    let formDiv = document.getElementById("formDiv")
+    formDiv.innerHTML = "<p class ='important'>Usuari registrat correctament</p>"
+    return false;
+    
 }
