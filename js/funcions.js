ca0f3ea0ae92b7567e74f4fafd6bf9ca69530853
MCarmen <detoro.mcarmen@gmail.com>
2025-10-17 11:06:56 +0200

    First version

---
+++ js/funcions.js
@@ -0,0 +1,24 @@
+function confirmaRegistre(){
+    alert("registrant estudiant");
+    document.getElementById("formDiv").innerHTML = "<p class='important'>T'has registrat amb èxit!</p>";
+    console.log("registrant estudiant");
+    return false;
+}
+
+/*
+async function carregaMencions(){
+    var tagGraus = document.getElementById("graus");
+    var resposta = await fetch("mencions.php?grau="+tagGraus.value);
+    var respostaTxt = await resposta.text();
+    document.getElementById("mencions").innerHTML = respostaTxt;
+}
+*/
+
+$(document).ready(function(){
+    $("#graus").change(function(){
+        $.ajax({url: "mencions.php?grau=" + $("#graus").val(), success:
+        function(result){
+            $("#mencions").html(result);
+        }});
+    });
+});
